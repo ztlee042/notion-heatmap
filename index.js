7d9707204bc626806ed7fcfd20ed6b721a97c6ca
ztlee042 <leeemail9898@gmail.com>
2023-03-05 23:28:16 +0100

    Update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 8000
 const app = express()
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+})
 
 app.get('/pomos', async (req, res) => {
   const pomos = await getPomos()
